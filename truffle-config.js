736273103b0af40c0921a22985fd9c4630ccb7e4
Patrik Åkerlund <akerlund.patrik@gmail.com>
2020-03-24 09:29:43 +0100

    Add JAMBOPAY-KSH token and zippienet support (to deploy with truffle)

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,29 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-};
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
+    zippienet: {
+      provider: () =>
+        // Change pivatekey to account to deploy from
+        new HDWalletProvider(
+          '0000000000000000000000000000000000000000000000000000000000000000',
+          'https://zippienet-eth.dev.zippie.org/rpc',
+          0,
+          1
+        ),
+      network_id: '*' // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: '0.4.26'
+    }
+  }
+}
